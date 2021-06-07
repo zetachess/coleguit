395035a2782a1696365821befc744514193f5340
nickolay <nickolay@agritask.com>
2021-06-07 22:33:27 +0300

    bash scripts

---
+++ bash-scripts/rename.arenas.sh
@@ -0,0 +1,46 @@
+####################################
+# arguments:
+key=$1
+dir_with_jsons=$2
+
+####################################
+# schedule related variables:
+. ./schedule.init.sh
+
+
+####################################
+# renaming:
+prevTournamentId="z"
+idx=$((numoftour-1))
+prevTC="z"
+
+for filename in `ls ./$dir_with_jsons/*.json | sort -gr`; do 
+    echo "-- $filename -----------------------------------------------------------------"; 
+    tournamentId=$(eval cat ${filename} | jq -r .id); 
+    echo $tournamentId
+    description="Full list of existing tournaments: [https://iwantzh.github.io/list.html](https://iwantzh.github.io/list.html)%0A%0AAlways one 24/7"
+    if [ "$prevTournamentId" == 'z' ]
+    then
+        description="Full list of existing tournaments: [https://iwantzh.github.io/list.html](https://iwantzh.github.io/list.html)%0A%0AAlways one 24/7"
+    else
+        description="Full list of existing tournaments: [https://iwantzh.github.io/list.html](https://iwantzh.github.io/list.html)%0A%0A[Next+UZH+Arena:+$prevTC](https://lichess.org/tournament/$prevTournamentId)"
+    fi
+    #echo $description
+
+	actualCmd="curl -s -X POST -H \"Authorization: Bearer $key\" -d 'variant=crazyhouse&clockTime=${clockTime[idx]}&clockIncrement=${clockIncr[idx]}&minutes=${durminute[idx]}&description=$description' https://lichess.org/api/tournament/$tournamentId"
+
+	echo "$actualCmd"
+
+	#########################################################
+	#update tournament description - get json response
+
+	responseJson=$(eval $actualCmd)
+
+	echo $'\e[1;33m'$responseJson$'\e[0m'
+
+
+    prevTournamentId=$tournamentId
+    prevTC="${clockTime[idx]}%2b${clockIncr[idx]}"
+    idx=$((idx-1))
+done
+
